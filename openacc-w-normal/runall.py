8cfc47fd91ad9669ff618a35cfe634de29909461
milladgit <millad.mg@gmail.com>
2019-02-04 16:51:34 -0800

    fixing run files

--- openacc-w-normal/runall.py
+++ openacc-w-normal/runall.py
@@ -9,7 +9,7 @@ def get_name(filename):
 	return s[::-1]
 
 
-gecko_config_folder_base = "/home/millad/gecko-configs/"
+gecko_config_folder_base = "/home/mghane/gecko-configs/"
 
 
 config_files = glob.glob(gecko_config_folder_base + "*")
@@ -18,6 +18,8 @@ config_files = sorted(config_files)
 cwd = os.getcwd()
 
 app_list = ["bfs", "cfd", "gaussian", "hotspot", "lavaMD", "lud", "nn", "nw", "particlefilter", "pathfinder", "srad_v2"]
+app_list = ["gaussian", "particlefilter"]
+
 
 # file_list = glob.glob("%s/*" % (cwd))
 file_list = ["%s/%s" % (cwd, f) for f in app_list]
